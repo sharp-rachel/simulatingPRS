e4f31558c69a608cda74ff28bafb364bfeb25634
sharp-rachel <rrsharp122@gmail.com>
2024-04-03 13:55:15 -0400

    Notes from meeting with Mike 4/3/24 on setting up simulations

--- scripts/01_PRSSimulations.R
+++ scripts/01_PRSSimulations.R
@@ -26,9 +26,23 @@ library(ggplot2)
 library(tibble)
 
 # make cases
-prob <- logistic(-4 + .5 * sdoh_risk + pos(.5 * prs)) # prob of having the disorder
+# can play with -4 to make baseline odds match that of bpd
+# if prevalnce of bpd is 10% do exp(-4) where I replace -4 with diff numbers until I reach 10%
+# NOT PREVALANCE - odds
+prob <- logistic(-4 + .5 * sdoh_risk + pos(.5 * prs) + sdoh_risk * pos(.5 * prs)) # prob of having the disorder #add interaction of sdoh and prs -4 makes logodds e to the -4
+#eun line 34 1,000 times, count how many times the interaction term was significant, that's the power
 case <- rbinom(n, size=1, prob=prob) # Using prob to define true cases
 fit <- glm(case ~ prs, family=binomial)
 dat <- tibble(prs, sdoh_risk, ancestry, case)
+
+dat |>
+  mutate(prs_bin = cut_fun(prs, 10),
+         sdoh_risk_bin = cut_fun(sdoh_risk, 4)) |>
+  group_by(prs_bin, sdoh_risk_bin) |>
+  summarize(prevalence = mean(case), n = n()) |>
+  ggplot(aes(prs_bin, prevalence, color=sdoh_risk_bin, group=sdoh_risk_bin)) +
+  geom_point() +
+  geom_line() +
+  ggtitle("no interaction, just shift")
 
 write.csv(dat, "data/simulated_prs/mike_examples/03_29_24.csv", row.names = F)
--- scripts/02_modelComparisons.R
+++ scripts/02_modelComparisons.R
@@ -7,7 +7,6 @@ library(tidyverse)
 
 prs.dat <- read.csv("data/simulated_prs/mike_examples/03_29_24.csv")
 
-
 # Try glm with simple model
 
 # Define functions for case probabilities
@@ -23,5 +22,23 @@ model3 <- glm(data = prs.dat, case ~ prs + ancestry + sdoh_risk, family = binomi
 summary(model3)
 
 # Make it interaction with ancestry and sdoh
-model4 <- glm(data = prs.dat, case ~ prs + ancestry + sdoh_risk + ancestry:sdoh_risk, family = binomial)
+model4 <- glm(data = prs.dat, case ~ prs + ancestry + sdoh_risk + prs:sdoh_risk, family = binomial)
 summary(model4)
+
+#Was the interaction term significant - repated = senstivity 
+# in how many sims was the coefficient sig?
+# we should have X power in our sample size to detect x effect size
+
+##Iterate through sample size, effect size, correlation
+##Step-wise thresholding effects?
+
+
+###
+#outside loop where I iterate through effect sizes -> sapply([0.01, 0.02, 0.03])
+#inisde loop
+# sig <- replicate(10,000{
+#  rbinom
+#  glm()
+#  logical -> pval < 0.05
+#})
+#mean(sig)
